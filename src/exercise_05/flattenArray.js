a910f283cf9cf8b10de28f25b6dde46d182c197e
tanghaoran <407458281@qq.com>
2020-09-02 17:02:48 +0800

    [thr] feat: modify exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,10 +1,23 @@
-export default function flattenArray(array) {
+export default function flattenArray(array, result = []) {
   // This function flattens a nested array into a sequence.
   //
   // Your target:
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if(array == undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+  for(let item of array){
+    if(Array.isArray(item)){
+      // flattenArray(item, result);
+      for(let it of item){
+        result.push(it);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+  
 }
