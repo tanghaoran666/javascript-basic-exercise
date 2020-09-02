88f0fdb56ab120ecdfd559fdce7ce8710917e8e1
tanghaoran <407458281@qq.com>
2020-09-02 20:41:11 +0800

    [thr] feat: modify exercise_06 hardly

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -12,5 +12,50 @@ export default class Vector {
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  set x(value) {
+    if (this.x === undefined) {
+      this._x_ = value;
+    } else {
+      throw new Error();
+    }
+  }
+
+  set y(value) {
+    if (this.y === undefined) {
+      this._y_ = value;
+    } else {
+      throw new Error();
+    }
+  }
+
+  get x() {
+    return this._x_;
+  }
+
+  get y() {
+    return this._y_;
+  }
+
+  static plus(vectorA, vectorB) {
+    const x = vectorA.x + vectorB.x;
+    const y = vectorA.y + vectorB.y;
+    const vectorResult = new Vector(x, y);
+    return vectorResult;
+  }
+
+  static minus(vectorA, vectorB) {
+    const x = vectorA.x - vectorB.x;
+    const y = vectorA.y - vectorB.y;
+    const vectorResult = new Vector(x, y);
+    return vectorResult;
+  }
+
+  distance() {
+    return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+  }
 }
