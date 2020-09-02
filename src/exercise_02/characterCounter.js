25e3154e942639ec085f2e80b275fa38b4d4ddab
tanghaoran <407458281@qq.com>
2020-09-02 15:18:34 +0800

    [thr] feat: modify exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,18 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (string == undefined){
+    return 0;
+  }
+  if (prediction === undefined) {
+    return string.length;
+  } else {
+    console.log("Debug: " + prediction());
+    var appear = 0;
+    for(var i =0;i < string.length;i++){
+      if (prediction(string[i])){appear += 1;}
+    }
+    return (appear);
+    // return 2;
+  }
 }
